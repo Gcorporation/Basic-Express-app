64bf6216855f7e7f891c87d1fc4c7857b55da613
Prabhjot Singh <prabhjotsingh394@gmail.com>
2023-03-05 18:48:26 +0530

    adding a 404 error page

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.use(adminRoutes);
 
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not Found!</h1>')
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
