c8c4b5607cc1c008d1c1cebb7d4ed60c94a84eac
Prabhjot Singh <prabhjotsingh394@gmail.com>
2023-03-08 11:19:30 +0530

    serving static files

--- routes/admin.js
+++ routes/admin.js
@@ -2,17 +2,19 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../utils/path');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.get('/add-product',(req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,13 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../utils/path');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
-
 
 module.exports = router;
